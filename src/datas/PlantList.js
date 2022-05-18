076a88344e97a8cde3608a8383bdf23e05ef338a
PaolaGraciella <paolagraciellafonkou@gmail.com>
2022-05-18 22:22:23 +0200

    GardenStore

---
+++ src/datas/PlantList.js
@@ -0,0 +1,98 @@
+import cover from '../assets/cover.jpg'
+
+
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+        isBestSale : true,
+        isSpecialOffer : true,
+        water: 3,
+        light : 1,
+        cover : cover,
+        price: 15
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+        isBestSale : true,
+        water: 3,
+        light : 1,
+        cover : cover,
+        price: 20
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+        isBestSale : false,
+        isSpecialOffer : true,
+        water: 3,
+        light : 3,
+        cover : cover,
+        price: 15
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk',
+        isBestSale : true,
+        water: 3,
+        light : 2,
+        cover : cover,
+        price: 14
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+        isBestSale : true,
+        water: 2,
+        light : 3,
+        cover : cover,
+        price: 15
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo',
+        isBestSale : false,
+        water: 1,
+        light : 2,
+        cover : cover,
+        price: 8
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+        isBestSale : false,
+        isSpecialOffer : true,
+        water: 2,
+        light : 1,
+        cover : cover,
+        price: 17
+	},
+	{
+		name: 'aloe',
+		category: 'plante grasse',
+		id: '8fp',
+        isBestSale : true,
+        water: 2,
+        light : 2,
+        cover : cover,
+        price: 10
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+        isBestSale : false,
+        water: 1,
+        light : 3,
+        cover : cover,
+        price: 18
+	}
+]
